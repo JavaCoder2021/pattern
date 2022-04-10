088e4bc6e60ba52964760435c7e64333529fab79
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-04-10 15:52:54 +0200

    Add files via upload

---
+++ template_method/Coffee.java
@@ -0,0 +1,15 @@
+package by.epam.pattern.template_method;
+
+public class Coffee extends CaffeineBeverage {
+
+	@Override
+    public void brew() {
+        System.out.println("Dripping Coffee through filter");
+    }
+
+	@Override
+    public void addCondiments() {
+        System.out.println("Adding Sugar and Milk");
+    }
+    
+}
---
+++ template_method/Tea.java
@@ -0,0 +1,15 @@
+package by.epam.pattern.template_method;
+
+public class Tea extends CaffeineBeverage {
+	
+	@Override
+    public void brew() {
+        System.out.println("Steeping the tea");
+    }
+
+	@Override
+    public void addCondiments() {
+        System.out.println("Adding Lemon");
+    }
+    
+}
