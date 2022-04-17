1292c05bbbd1a43e029fd5b398ec98e682e81616
Igor <92649923+JavaCoder2021@users.noreply.github.com>
2022-04-17 16:57:03 +0200

    Add files via upload

---
+++ composite/NullIterator.java
@@ -0,0 +1,19 @@
+package by.epam.pattern.composite;
+
+import java.util.Iterator;
+
+public class NullIterator implements Iterator {
+
+	public Object next() {
+		return null;
+	}
+
+	public boolean hasNext() {
+		return false;
+	}
+
+	public void remove() {
+		throw new UnsupportedOperationException();
+	}
+
+}
